1dd258a086a1c9c6029aec1fa7bcf7af78d1e2dd
Sandesh2868 <sandy2868kulkarni@gmail.com>
2025-07-31 16:13:34 +0530

    Update HeroBackground.jsx

--- src/components/HeroBackground.jsx
+++ src/components/HeroBackground.jsx
@@ -4,13 +4,17 @@ import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
 function SymbolSVG() {
   const data = useLoader(SVGLoader, "/symbol.svg");
-  const shapes = useMemo(() => data.paths.flatMap((path) => path.toShapes(true)), [data]);
+  const shapes = useMemo(
+    () => data.paths.flatMap((path) => path.toShapes(true)),
+    [data]
+  );
+
   return (
     <group scale={[0.018, -0.009, 0.018]} position={[-10, 2, -4]}>
       {shapes.map((shape, i) => (
         <mesh key={i}>
           <shapeGeometry args={[shape]} />
-          <meshBasicMaterial color="#4fc3f7" opacity={0.15} transparent />
+          <meshBasicMaterial color="#4fc3f7" opacity={0.1} transparent />
         </mesh>
       ))}
     </group>
@@ -20,11 +24,11 @@ function SymbolSVG() {
 export default function HeroBackground() {
   return (
     <div className="absolute top-0 left-0 w-full h-full z-0 overflow-hidden">
-      <Canvas className="w-full h-full">
+      <Canvas className="pointer-events-none w-full h-full">
         <ambientLight intensity={0.3} />
         <directionalLight position={[0, 5, 5]} intensity={0.5} />
         <SymbolSVG />
       </Canvas>
-     </div>
+    </div>
   );
 }
